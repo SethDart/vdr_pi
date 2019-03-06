5ca9040b1f2f66cb712951c61661b9949be6acd6
rasbats <miker@members.swis.net>
2019-03-06 15:32:55 +0000

    Update for ov50

--- build/CMakeFiles/vdr-po-update.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vdr-po-update"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vdr-po-update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/vdr-pot-update.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vdr-pot-update"
-  "po/vdr_pi.pot.dummy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vdr-pot-update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
