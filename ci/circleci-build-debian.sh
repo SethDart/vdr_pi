d2e1333a9150daca3cc43945f33e1075ce1979c1
Rick Gleason <fcgleason@tgp-architects.com>
2020-03-09 11:11:59 -0400

    ci:circleci-build-debian: Add wxgtk support

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -7,19 +7,28 @@ set -xe
 sudo apt-get -qq update
 sudo apt-get install devscripts equivs
 
-rm -rf build && mkdir build && cd build
+#rm -rf build && mkdir build && cd build
+mkdir build
+cd build
 mk-build-deps ../ci/control
 sudo apt-get install  ./*all.deb  || :
 sudo apt-get --allow-unauthenticated install -f
-rm -f ./*all.deb
+# rm -f ./*all.deb
 
-tag=$(git tag --contains HEAD)
-
-if [ -n "$tag" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Release ..
-else
-  cmake -DCMAKE_BUILD_TYPE=Debug ..
+if [ -n "$BUILD_GTK3" ]; then
+    sudo update-alternatives --set wx-config \
+        /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
 fi
+
+#tag=$(git tag --contains HEAD)
+#
+#if [ -n "$tag" ]; then
+#  cmake -DCMAKE_BUILD_TYPE=Release ..
+#else
+#  cmake -DCMAKE_BUILD_TYPE=Debug ..
+#fi
+
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 
 make -j2
 make package
