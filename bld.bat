3650045b5b3bf69936369d08f2f42dec6df40d01
Rick Gleason <fcgleason@tgp-architects.com>
2021-07-09 15:31:09 -0400

    v1.2.10.0 update from shipdriver - not working

--- bld.bat
+++ bld.bat
@@ -1,4 +1,5 @@
 cd build
 cmake -T v141_xp ..
-cmake --build . --target package --config release 
-bash ./cloudsmith-upload.sh
+cmake --build . --target package --config release >output.txt
+rem cd build
+rem bash ./cloudsmith-upload.sh
