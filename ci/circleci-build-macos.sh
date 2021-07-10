f5592e20f8ea0e507d799af71bf9f2a72152c251
Rasbats <miker@members.swis.net>
2021-07-10 11:12:30 +0100

    Initial import of shipdriver directories

---
+++ ci/circleci-build-macos.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+
+#
+# Build the  MacOS artifacts
+
+set -xe
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+# Return latest version of $1, optiomally using option $2
+pkg_version() { brew list --versions $2 $1 | tail -1 | awk '{print $2}'; }
+
+#
+# Check if the cache is with us. If not, re-install brew.
+brew list --versions libexif || brew update-reset
+
+# Install packaged dependencies
+here=$(cd "$(dirname "$0")"; pwd)
+for pkg in $(sed '/#/d' < $here/../build-deps/macos-deps);  do
+    brew list --versions $pkg || brew install $pkg || brew install $pkg || :
+    brew link --overwrite $pkg || brew install $pkg
+done
+
+if brew list --cask --versions packages; then
+    version=$(pkg_version packages '--cask')
+    sudo installer \
+        -pkg /usr/local/Caskroom/packages/$version/packages/Packages.pkg \
+        -target /
+else
+    brew install --cask packages
+fi
+
+# Install the pre-built wxWidgets package
+wget -q https://download.opencpn.org/s/rwoCNGzx6G34tbC/download \
+    -O /tmp/wx312B_opencpn50_macos109.tar.xz
+tar -C /tmp -xJf /tmp/wx312B_opencpn50_macos109.tar.xz 
+
+
+# Build and package
+rm -rf build && mkdir build && cd build
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312B_opencpn50_macos109/bin/wx-config \
+  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312B_opencpn50_macos109" \
+  -DCMAKE_INSTALL_PREFIX= \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  ..
+
+if [[ -z "$CI" ]]; then
+    echo '$CI not found in environment, assuming local setup'
+    echo "Complete build using 'cd build; make tarball' or so."
+    exit 0 
+fi
+
+make VERBOSE=1 tarball
+
+make pkg    
+
+# Install cloudsmith needed by upload script
+python3 -m pip install --user cloudsmith-cli
+
+# Required by git-push
+python3 -m pip install --user cryptography
+
+# python3 installs in odd place not on PATH, teach upload.sh to use it:
+pyvers=$(python3 --version | awk '{ print $2 }')
+pyvers=$(echo $pyvers | sed -E 's/[\.][0-9]+$//')    # drop last .z in x.y.z
+echo "export PATH=\$PATH:/Users/distiller/Library/Python/$pyvers/bin" \
+    >> ~/.uploadrc
---
+++ cmake/PluginCompiler.cmake
@@ -0,0 +1,68 @@
+# ~~~
+# Author:      Pavel Kalian (Based on the work of Sean D'Epagnier)
+# Copyright:   2014
+# License:     GPLv3+
+#
+# Set up the compilation environment, compiler options etc.
+# ~~~
+
+
+# Set up option NPROC: Number of processors used when compiling.
+if (DEFINED ENV{CMAKE_BUILD_PARALLEL_LEVEL})
+  set(_nproc $ENV{CMAKE_BUILD_PARALLEL_LEVEL})
+else ()
+  if (WIN32)
+    set(_nproc_cmd
+      cmd /C if defined NUMBER_OF_PROCESSORS (echo %NUMBER_OF_PROCESSORS%)
+      else (echo 1)
+    )
+
+  elseif (APPLE)
+    set(_nproc_cmd sysctl -n hw.physicalcpu)
+  else ()
+    set(_nproc_cmd nproc)
+  endif ()
+  execute_process(
+    COMMAND ${_nproc_cmd}
+    OUTPUT_VARIABLE _nproc
+    RESULT_VARIABLE _status
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if (NOT "${_status}" STREQUAL "0")
+    set(_nproc 1)
+    message(
+      STATUS "Cannot probe for processor count using \"${_nproc_cmd}\""
+    )
+  endif ()
+endif ()
+set(OCPN_NPROC ${_nproc}
+  CACHE STRING "Number of processors used to compile [${_nproc}]"
+)
+message(STATUS "Build uses ${OCPN_NPROC} processors")
+
+set(_ocpn_cflags " -Wall -Wno-unused-result -fexceptions")
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  string(APPEND CMAKE_C_FLAGS " ${_ocpn_cflags}")
+  string(APPEND CMAKE_CXX_FLAGS " ${_ocpn_cflags}")
+  string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,-Bsymbolic")
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")           # Apple is AppleClang
+  string(APPEND CMAKE_C_FLAGS " ${_ocpn_cflags}")
+  string(APPEND CMAKE_CXX_FLAGS " ${_ocpn_cflags}")
+  string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl -undefined dynamic_lookup")
+  string(APPEND CMAKE_CXX_FLAGS " ${_ocpn_cflags} -Wno-inconsistent-missing-override")
+  string(APPEND CMAKE_CXX_FLAGS
+        " ${_ocpn_cflags} -Wno-potentially-evaluated-expression"
+  )
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  add_definitions(-D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_SECURE_NO_DEPRECATE)
+endif ()
+
+if (UNIX AND NOT APPLE)   # linux, see OpenCPN/OpenCPN#1977
+  set_target_properties(${PACKAGE_NAME}
+    PROPERTIES INSTALL_RPATH "$ORIGIN:$ORIGIN/.."
+  )
+endif ()
+
+if (MINGW)
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L../buildwin")
+endif ()
